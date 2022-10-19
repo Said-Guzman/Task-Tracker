f36d6b1e66a348f508fe2cc56aecbe95978deb70
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-10-19 09:53:10 -0400

    updates

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //UI elements
-const taskInput = document.querySelector('.task-input')
+const taskInput = document.querySelector('#task-input')
 const form = document.querySelector('#task-form')
 const taskSubmit = document.querySelector("#task-submit")
-const taskList = document.getElementById("tasks") 
-const erase = document.getElementById("delete")
+const taskList = document.getElementById('tasks') 
+
 
 allEventListeners()
 
@@ -12,36 +12,39 @@ function allEventListeners(){
     form.addEventListener('submit', addTask)
 
     //remove task
-
+    taskList.addEventListener('click',removeTask)
 }
 // adds tasks
 function addTask(e){
-    if(taskInput.value === ''){
-        alert('Add a Task Please')
-
+    if(taskInput.value === ""){
+        alert('Add a Task Please');
+            return;
+            
+} 
         // create li element
-        const li = document.createElement('li')
-        li.className = 'item'
-        li.appendChild(document.createTextNode(taskInput.value))
+        const li = document.createElement("li");
+        li.className = 'tasks';
+        li.appendChild(document.createTextNode(taskInput.value));
 
         //create an element
         const a = document.createElement('a')
         a.className = 'delete'
-        a.innerHTML = '<i>Delete</i>'
+        a.innerHTML = ' &nbsp &nbsp <i>Delete</i>'
 
         li.appendChild(a)
-        console.log(li)
 
         //append li to ul 
         taskList.append(li)
 
         e.preventDefault()
-
-    }
+    
 }
 
-
-
+function removeTask(e){
+   if(e.target.parentElement.classList.contains('delete')){
+    e.target.parentElement.parentElement.remove()
+   } 
+}
 
 
 
