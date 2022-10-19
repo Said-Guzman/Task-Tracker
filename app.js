5462cf89e0b3f17d7da8a8818972a967eaa85606
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-10-19 15:22:20 -0400

    updated

--- app.js
+++ app.js
@@ -8,12 +8,47 @@ const taskList = document.getElementById('tasks')
 allEventListeners()
 
 function allEventListeners(){
+
+    //DOM Content Loader
+    document.addEventListener('DOMContentLoaded', getTasks)
+
     //add task
     form.addEventListener('submit', addTask)
 
     //remove task
     taskList.addEventListener('click',removeTask)
 }
+    
+//get task from local storage
+    function getTasks(){
+        let tasks
+        if(localStorage.getItem('tasks') === null){
+            tasks = []
+        }
+        else{
+            tasks = JSON.parse(localStorage.getItem('tasks'))
+        }
+        for (let task of tasks){
+            const li = document.createElement("li");
+            li.className = 'tasks';
+            li.appendChild(document.createTextNode(task));
+    
+            //create an element
+            const a = document.createElement('a')
+            a.className = 'delete'
+            a.innerHTML = ' &nbsp &nbsp <i></i>'
+    
+            li.appendChild(a)
+    
+            //append li to ul 
+            taskList.append(li)
+        }
+    }
+
+
+
+
+
 // adds tasks
 function addTask(e){
     if(taskInput.value === ""){
@@ -29,30 +64,69 @@ function addTask(e){
         //create an element
         const a = document.createElement('a')
         a.className = 'delete'
-        a.innerHTML = ' &nbsp &nbsp <i>Delete</i>'
+        a.innerHTML = ' &nbsp &nbsp <i></i>'
 
         li.appendChild(a)
-
+ 
         //append li to ul 
         taskList.append(li)
+
+
+        //add task to local storage
+        addLocal(taskInput.value)
+
 
         e.preventDefault()
     
 }
 
+    function addLocal(task){
+        let tasks
+            if(localStorage.getItem('tasks') === null){
+                tasks = []
+            }
+        else{
+            tasks = JSON.parse(localStorage.getItem('tasks'))
+            }
+            tasks.push(task)
+            
+            localStorage.setItem('tasks', JSON.stringify(tasks))
+        }   
+
+
 function removeTask(e){
    if(e.target.parentElement.classList.contains('delete')){
     e.target.parentElement.parentElement.remove()
+
+    removeLocal(e.target.parentElement.parentElement)
+
    } 
 }
+    function clearTask(){
+        taskList.innerHTML = ''
+    }
 
 
 
 
+ //remove from the local storage
+ function removeLocal(taskItem){
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    }
+    else{
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === tasks){
+            task.splice(1, tasks)
+        }
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    })
 
-
-
-
+ }
+  
 
 
 
