25fe435614756ae18995136a4d8fa775a8752b55
Said-Guzman <112099452+Said-Guzman@users.noreply.github.com>
2022-10-19 07:35:38 -0400

    updates

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 //UI elements
-const taskInput = document.getElementById("task-input")
-const taskSubmit = document.getElementById("task-submit")
+const taskInput = document.querySelector('.task-input')
+const form = document.querySelector('#task-form')
+const taskSubmit = document.querySelector("#task-submit")
 const taskList = document.getElementById("tasks") 
 const erase = document.getElementById("delete")
 
 allEventListeners()
 
 function allEventListeners(){
-    FormData.addEventListener('submit', addTask)
-}
+    //add task
+    form.addEventListener('submit', addTask)
 
+    //remove task
+
+}
+// adds tasks
 function addTask(e){
     if(taskInput.value === ''){
         alert('Add a Task Please')
+
+        // create li element
+        const li = document.createElement('li')
+        li.className = 'item'
+        li.appendChild(document.createTextNode(taskInput.value))
+
+        //create an element
+        const a = document.createElement('a')
+        a.className = 'delete'
+        a.innerHTML = '<i>Delete</i>'
+
+        li.appendChild(a)
+        console.log(li)
+
+        //append li to ul 
+        taskList.append(li)
+
+        e.preventDefault()
+
     }
 }
 
