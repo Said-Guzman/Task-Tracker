93674f0458b2d9b3bcc2dba0f008a6c8921e6241
Royce Chen <94400272+chen-royce@users.noreply.github.com>
2022-10-22 00:48:47 -0400

    Add mock data, update/use server instead of app

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ import express from "express"
 
 
 const app = express()
+const baseData = fs.readFile("./data.json", (err, data) => {
+    console.log(JSON.parse(data))
+})
 
-app.get("/", (req,res)=>{
-    fs.readFile("./data.json", (err, data) =>{
-        if(err) return err
+app.get("/", (req, res) => {
+    fs.readFile("./data.json", (err, data) => {
+        if (err) return err
         res.json(JSON.parse(data))
     })
 })
 
 
-app.listen(4000, (err) =>{
+app.listen(4000, (err) => {
     if (err) return err;
     console.log(`running on port 4000..`)
 })
 
- 
+
 // module.exports = "./data.json"
-
-
-
-
